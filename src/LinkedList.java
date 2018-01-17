c5e7067de7ab218164d23e79a717d5d2b97e853c
Matthew Leong <mleong@skidmore.edu>
2018-01-17 15:47:24 -0500

    Added a add element function to linkedlist

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -1,4 +1,52 @@
-package PACKAGE_NAME;
+public class LinkedList <E>{
+    //private nodes that define the linkedlist. Temp will be for holding nodes
+    //head is the first node and tail is the last
+    private Node<E> head = null;
+    private Node<E> tail = null;
+    private Node<E> temp = null;
+    //counter is used to make the size method O(1) instead of O(n) by iterating through a list
+    private int counter = 0;
 
-public class LinkedList {
+    //constructor
+    public LinkedList(){
+
+    }
+
+    //methods
+    /*Method: size()
+      Summary: returns the size of a linked list by returning it's counter private variable.  Since this
+      is incremented and decremented inside methods, the time for this method is O(1).
+     */
+    public int size(){
+        return counter;
+    }
+
+    /*Good tip for writing methods for a linkedlist.  You are only looking at only 3 nodes
+      when incorporating add, remove etc methods. The node, the one before it, and the one after.
+      If you figure out those relations the method should be easy.*/
+
+    /*Method: add()
+      Summary: adds an element to the linked list.  there are two cases. 1 that there is no node
+      already in the list in which head and tail are created to reference a new node. then head's elem
+      is set to the parameter. Head's next is set to tail but also tail is set to head since at the first
+      node head is also the tail. 2 if there is a node already it is set to the new tail. Counter is
+      incremented to reflect size change.
+      Param: elem, an element
+     */
+    public void add(E elem){
+        //if there are no elems in our linkedlist
+        if(head == null){
+            //Kind of instantiates the linkedlist. head and tail are made to be a new node
+            head = tail = new Node<E>();
+            //head's elem is set to the param
+            head.elem = elem;
+            head.next = tail;
+            tail = head;
+        } else {
+            tail.next = new Node<E>();
+            tail = tail.next;
+            tail.elem = elem;
+        }
+        counter ++;
+    }
 }
