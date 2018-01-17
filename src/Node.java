a039b52669631c57f0453f40936befa8111b26c5
Matthew Leong <mleong@skidmore.edu>
2018-01-17 15:24:24 -0500

    Initial commit of node class

---
+++ src/Node.java
@@ -0,0 +1,7 @@
+public class Node<E> {
+    //elem variable is of the generic type
+    //a node has a reference to its next and previous nodes for a doubly linked list
+    E elem;
+    Node<E> next;
+    Node<E> prev;
+}
