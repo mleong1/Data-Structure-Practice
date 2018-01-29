9f2815027f2ffc6bcc75ea1b1e9ee50d27645306
Matthew Leong <mleong@skidmore.edu>
2018-01-29 17:04:16 -0500

    Finished hashtable for string values

--- src/HashTables/HashTable2.java
+++ src/HashTables/HashTable2.java
@@ -9,7 +9,42 @@ public class HashTable2 {
     //public variables
     WordList[] hashArray;
     int arraySize;
+    public String[][] elementsToAdd = {
+            {"ace", "Very good"},
+            {"act", "Take action"},
+            {"add", "Join something to something else"},
+            {"age", "Grow old"}};
+    
+    //public constructor
+    public HashTable2(int size){
+        arraySize = size;
+        hashArray = new WordList[size];
 
+        for(int i = 0; i < arraySize; i++){
+            hashArray[i] = new WordList();
+        }
+        addTheArray(elementsToAdd);
+    }
+
+    public void insert(Word newWord){
+        //store the string of the actual word
+        String wordToHash = newWord.theWord;
+        int hashKey = stringHashFunction(wordToHash);
+        //insert into the hashArray at the hashKey the newWord
+        hashArray[hashKey].addToList(newWord, hashKey);
+
+    }
+
+    public void addTheArray(String [][] elementsToAdd){
+        for(int i = 0; i < elementsToAdd.length; i ++){
+            String word = elementsToAdd[i][0]; //whatever index the first dimension is the word
+            String definition = elementsToAdd[i][1]; //2nd dimension is the definition
+
+            //construct a word object with this
+            Word newWord = new Word(word, definition);
+            insert(newWord);
+        }
+    }
     //hash function cycles through the letters in the word and creates a hash key based on equation. then you mod
     //the hash value by the array size giving you the index for the table.
 
@@ -29,6 +64,12 @@ public class HashTable2 {
             hashKeyVal = (hashKeyVal * 27 + charCode) % arraySize;
         }
         return hashKeyVal;
+    }
+
+    public void displayTheArray(){
+        for(int i = 0; i < arraySize; i ++){
+            hashArray[i].displayWordList();
+        }
     }
 }
 
@@ -63,10 +104,50 @@ class WordList{
     //public variable
     public Word firstWord = null;
 
-    public addToList(Word newWord, int HashKey){
+    public void addToList(Word newWord, int hashKey){
 
         Word previous = null;
         Word current = firstWord;
+
+        newWord.key = hashKey;
+
+        //this will add words to the list in order
+        while(current != null && newWord.key > current.key){
+            //cycle through words in the list
+            previous = current;
+            current = current.next;
+        }
+        //if the hashkey of the firstWord is less than the hashkey of the newWord
+        if(previous == null){
+            firstWord = newWord;
+        //else newWord belongs in the space after previous but before current
+        } else {
+            previous.next = newWord;
+        }
+        //newWord should be placed correctly now in the list. need to set its next.
+        newWord.next = current;
     }
 
+    public void displayWordList(){
+        Word current = firstWord;
+        while(current != null){
+            System.out.println(current.toString());
+            current = current.next;
+        }
+    }
+
+    /*public Word find(int hashKey){
+        //dont forget words are in order
+        Word current = firstWord;
+        while(current != null && current.key <= hashKey){
+            if(current.key)
+        }
+    }*/
+
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        HashTable2 wordHashTable = new HashTable2(11);
+        wordHashTable.displayTheArray();
+    }
 }
