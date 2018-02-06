8b8fa9324b3796a1a183301a5c64cd551e789191
Matthew Leong <mleong@skidmore.edu>
2018-02-06 14:02:11 -0500

    Fixed the delete node function in btree

--- src/BinarySearchTrees/BinaryTree.java
+++ src/BinarySearchTrees/BinaryTree.java
@@ -1,5 +1,7 @@
 package BinarySearchTrees;
 
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
 public class BinaryTree {
     /*This is the class that organizes the treenodes into the binary tree data structure through means
       of addNode and deleteNode methods to enforce the shape.
@@ -115,12 +117,10 @@ public class BinaryTree {
         //Todo can't delete the root node of the entire btree
         //could have retooled findNode to return the parent of the node being searched for so as to not repeat
         //while loop code
+        System.out.println(root.key + " this is the root of the tree");
         TreeNode delNode = root;
         TreeNode parent = null;
-        //can't delete a null node
-        if(root == null){
-            return false;
-        }
+
         while(delNode.key != key){
             parent = delNode; //let parent hold the last node
             if(delNode.key > key){
@@ -136,23 +136,34 @@ public class BinaryTree {
         //at this point we should have a parent to the node being deleted and the node itself
         //here we need to manage 3 cases
 
+        /*
         //Not happy with the design here patching in a way to delete the root of the entire btree
         if(parent == null){
             //we are deleting root node here
             //case 1 no children
             if(delNode.leftChild == null && delNode.rightChild == null){
                 delNode = null;
+                return true;
             } //case 2 1 child
             else if (delNode.leftChild != null && delNode.rightChild == null){
                 delNode.leftChild = this.root;
+                return true;
             } else if (delNode.leftChild == null && delNode.rightChild != null){
                 delNode.rightChild = this.root;
+                return true;
             }
-        }
+        } */
         //what side is the delNode relative to the parent
+
         boolean delNodIsLeft = true;
-        System.out.println(parent.key);
-        System.out.println(delNode.key);
+
+        if(parent == null){
+            parent = delNode;
+        }
+        //we do reach this line twice
+        System.out.println(delNode.key + " is being deleted");
+        System.out.println(delNode.leftChild);
+
         //what about the root? we need a separate part fo deleting the root
 
         if(parent.key > delNode.key){
@@ -163,39 +174,72 @@ public class BinaryTree {
 
         //case 1: no children, works
         if(delNode.leftChild == null && delNode.rightChild == null){
-            if(delNodIsLeft){
+            if(delNode == parent){
+                System.out.println("got here for the 75 recursive");
+                this.root = null;
+            } else if(delNodIsLeft){
                 parent.leftChild = null;
             } else {
                 parent.rightChild = null;
             }
             //case 2: 1 child
         } else if(delNode.leftChild != null && delNode.rightChild == null){
-            if(delNodIsLeft){
+            System.out.println("Got here one child on the left");
+            if(delNode == root){
+                //remember we actually have to say this.root in order to change the actual btree,
+                //otherwise just changing a variable in the method
+                this.root = delNode.leftChild;
+            } else if(delNodIsLeft){
                 parent.leftChild = delNode.leftChild;
             } else {
                 parent.rightChild = delNode.leftChild;
             }
 
         } else if (delNode.leftChild == null && delNode.rightChild != null){
-            if(delNodIsLeft){
+            System.out.println("Got here one child on the right");
+            if(delNode == root){
+                this.root = delNode.rightChild;
+            } else if(delNodIsLeft){
                 parent.leftChild = delNode.rightChild;
             } else {
                 parent.rightChild = delNode.rightChild;
             }
             //case 3: 2 children, needs work
         } else if (delNode.leftChild != null && delNode.rightChild != null){
+            System.out.println("got here 2 children");
             //minimum right subtree node
             TreeNode minRSTNode = delNode.rightChild;
+
             //find minimum in right subtree
             while(minRSTNode.leftChild != null){
                 minRSTNode = minRSTNode.leftChild;
             }
-            System.out.println(minRSTNode.key);
-            delNode = minRSTNode;
+            System.out.println(minRSTNode.key + " is the min value for replacement");
+
+            TreeNode replacement = new TreeNode(minRSTNode.key, minRSTNode.name);
+
+
+            if(delNode == root){
+                //todo fix
+                this.root = replacement;
+                replacement.leftChild = delNode.leftChild;
+                replacement.rightChild = delNode.rightChild;
+            } else if (delNodIsLeft){
+                parent.leftChild = replacement;
+                replacement.leftChild = delNode.leftChild;
+                replacement.rightChild = delNode.rightChild;
+            } else {
+                parent.rightChild = replacement;
+                //need to hook up replacement node with delNodes leftside and rightside
+                replacement.leftChild = delNode.leftChild;
+                replacement.rightChild = delNode.rightChild;
+
+            }
 
             //remove duplicate value
-            this.delNode(delNode.rightChild, minRSTNode.key);
 
+            this.delNode(delNode.rightChild, minRSTNode.key);
+            System.out.println("got here");
 
         }
 
@@ -215,6 +259,9 @@ public class BinaryTree {
         bT.addNode(5, "whatver");
         bT.addNode(45, "whatever");
         bT.addNode(75, "whatever");
+        bT.addNode(65, "whatevr");
+        bT.addNode(80, "whatver");
+
 
         bT.inOrderTraversal(bT.root);
         System.out.println(" ");
@@ -224,7 +271,7 @@ public class BinaryTree {
         System.out.println(bT.findNode(40).name);
         System.out.println(bT.findNode(1).name);
         System.out.println("new Line");
-        bT.delNode(bT.root, 50);
+        bT.delNode(bT.root, 10);
         System.out.println("new btree");
         bT.inOrderTraversal(bT.root);
 
