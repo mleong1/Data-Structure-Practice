f7a8ff6b0003c987f59159c058702b9f53f9a4ec
Matthew Leong <mleong@skidmore.edu>
2018-03-14 15:51:38 -0400

    toyed around with using hashmaps for the final encoding step'

--- src/HuffmanCoding/HuffTreeNode.java
+++ src/HuffmanCoding/HuffTreeNode.java
@@ -1,6 +1,8 @@
 package HuffmanCoding;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.PriorityQueue;
 
 public class HuffTreeNode implements Comparable {
@@ -57,6 +59,14 @@ public class HuffTreeNode implements Comparable {
         return forest;
     }
 
+    public static HashMap<Character, Integer> lookupTable(PriorityQueue<HuffTreeNode> forest){
+        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+        for (HuffTreeNode node: forest){
+            map.put(node.name, node.freq);
+        }
+        return map;
+    }
+
     public static void printHisto(PriorityQueue<HuffTreeNode> forest){
         for (HuffTreeNode node: forest) {
             System.out.println(node.toString());
@@ -86,11 +96,60 @@ public class HuffTreeNode implements Comparable {
         }
     }
 
+    public static void inOrderTraversal(HuffTreeNode root){
+        if(root != null) {
+            inOrderTraversal(root.leftChild);
+            System.out.println(root.toString());
+            inOrderTraversal(root.rightChild);
+        }
+    }
+
+    public static void preOrderTraversal(HuffTreeNode root){
+        if(root != null) {
+            System.out.println(root.toString());
+            preOrderTraversal(root.leftChild);
+            preOrderTraversal(root.rightChild);
+        }
+    }
+
+    /*
+    public String findNode(HuffTreeNode root, HashMap<Character, Integer> map, char tarName){
+        //Concat coding to the end of this string and return
+        String encoding = "";
+        int tarFreq = map.get(tarName);
+        //here we need a table to associate names with their frequencies
+        while(root.name != name){
+            if((root.freq - tarFreq) > tarFreq){
+                //it isn't enough to just say that if root freq is bigger than tarfreq go to left child
+                //consider a middle node with freq of 6 which is the combined frequencies of left and right children
+                //with frequencies of 3 each
+                root = root.leftChild;
+                encoding.concat("0");
+            } else if((root.freq - tarFreq) == tarFreq){
+                //has to be either the left child or the right child
+                if(root.leftChild.name == tarName){
+                    encoding.concat("0");
+                } else {
+                    encoding.concat("1");
+                }
+                //else if (root.freq - tarFreq) > tarFreq
+                //ahhhhh I see the problem here
+                //may abandon this method
+                //method may not be needed. maybe the huff encoding could be made on the fly and stored in a hashmap
+            } else {
+
+            }
+        }
+        return encoding;
+    }*/
+
     public static void main(String[] args) {
         PriorityQueue<HuffTreeNode> forest = getFrequency("go go gophers");
         printHisto(forest);
-        huffman(forest);
+        HashMap<Character, Integer> map = lookupTable(forest);
+        HuffTreeNode root = huffman(forest);
         printHisto(forest);
+        //preOrderTraversal(root);
         //System.out.println(forest.get(2).compareTo(forest.get(2)));
     }
 }
