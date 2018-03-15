a941b4cd8b008b3535544ce2d4e70773fcaed873
Matthew Leong <mleong@skidmore.edu>
2018-03-15 00:16:59 -0400

    Added abstract class to represent cards

---
+++ src/ObjectOrientedProblems/Card.java
@@ -0,0 +1,15 @@
+package ObjectOrientedProblems;
+
+public class Card {
+    /*2-10 are the numbered cards
+      11, 12, 13 are jack, queen, king
+      1 is th ace
+     */
+    protected int faceValue;
+    protected Suit suit;
+
+    public Card(int fV, Suit s){
+        this.faceValue = fV;
+        this.suit = suit;
+    }
+}
