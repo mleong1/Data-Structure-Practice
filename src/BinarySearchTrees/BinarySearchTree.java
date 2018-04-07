8f03aa7101fc6ea6bf22ba2add9bf878f16ac44c
Matthew Leong <mleong@skidmore.edu>
2018-04-06 21:22:18 -0400

    Implemented the delete node method from scratch

--- src/BinarySearchTrees/BinarySearchTree.java
+++ src/BinarySearchTrees/BinarySearchTree.java
@@ -111,6 +111,95 @@ public class BinarySearchTree<T> {
         }
     }
 
+    public boolean delNode(int key) {
+        BSTnode delNode = root;
+        BSTnode parent = null;
+
+        while(key != delNode.getKey()){
+            parent = delNode;
+            if(key < delNode.getKey()){
+                delNode = delNode.getLeftChild();
+            } else {
+                delNode = delNode.getRightChild();
+            }
+
+            if(delNode == null){
+                return false;
+            }
+        }
+
+        boolean delNodeIsLeft;
+        if(parent == null){
+            delNodeIsLeft = false;
+        } else if(delNode.getKey() < parent.getKey()){
+            delNodeIsLeft = true;
+        } else {
+            delNodeIsLeft = false;
+        }
+
+        //case 1: delNode has no children
+        if(delNode.getLeftChild() == null && delNode.getRightChild() == null){
+            if(parent == null){
+                root = null;
+            } else if(delNodeIsLeft){
+                parent.setLeftChild(null);
+            } else {
+                parent.setRightChild(null);
+            }
+        }
+        //case2: delNode has 1 child
+        else if(delNode.getLeftChild() != null && delNode.getRightChild() == null){
+            if(parent == null){
+                root = delNode.getLeftChild();
+            } else if(delNodeIsLeft){
+                parent.setLeftChild(delNode.getLeftChild());
+            } else {
+                parent.setRightChild(delNode.getLeftChild());
+            }
+        } else if(delNode.getLeftChild() == null && delNode.getRightChild() != null){
+            if(parent == null){
+                root = delNode.getRightChild();
+            } else if(delNodeIsLeft){
+                parent.setLeftChild(delNode.getRightChild());
+            } else {
+                parent.setRightChild(delNode.getRightChild());
+            }
+        }
+        //case 3: delNode has 2 children
+        else if(delNode.getLeftChild() != null && delNode.getRightChild() != null){
+            //get the minimum item in the right subtree
+            //now i remember the recursion in this deletion. to delete the min item after we use it as a replacement
+            BSTnode minRSTNode = delNode.getRightChild();
+            BSTnode parentRST = delNode;
+            while(minRSTNode.getLeftChild() != null){
+                parentRST = minRSTNode;
+                minRSTNode = minRSTNode.getLeftChild();
+            }
+
+            BSTnode replacement = new BSTnode(minRSTNode.getKey(), minRSTNode.getData());
+
+            if(minRSTNode.getKey() < parentRST.getKey()){
+                parentRST.setLeftChild(null);
+            } else {
+                parentRST.setRightChild(null);
+            }
+
+            if(parent == null){
+                root = replacement;
+                replacement.setLeftChild(delNode.getLeftChild());
+                replacement.setRightChild(delNode.getRightChild());
+            } else if(delNodeIsLeft){
+                parent.setLeftChild(replacement);
+                replacement.setLeftChild(delNode.getLeftChild());
+                replacement.setRightChild(delNode.getRightChild());
+            } else {
+                parent.setRightChild(replacement);
+                replacement.setLeftChild(delNode.getLeftChild());
+                replacement.setRightChild(delNode.getRightChild());
+            }
+        }
+        return true;
+    }
 
     public static void main(String[] args) {
         BinarySearchTree bst = new BinarySearchTree();
@@ -118,9 +207,13 @@ public class BinarySearchTree<T> {
         bst.addNode(1, 5);
         bst.addNode(10, 'c');
         bst.addNode(15, "YAHHH");
+        bst.addNode(7, "asdf");
         System.out.println(bst.findNode(10).getData());
         System.out.println(bst.findNode(5).getData());
         System.out.println(bst.findNode(15).getData());
+
+        bst.delNode(10);
+        System.out.println(bst.root.getRightChild().getKey());
 
     }
 }
