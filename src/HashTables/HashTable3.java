3fb2669bab3d3ed957197f57a0cf2aa2f8fbd65f
Matthew Leong <mleong@skidmore.edu>
2018-04-07 15:08:16 -0400

    Added an add method for the hashtable

--- src/HashTables/HashTable3.java
+++ src/HashTables/HashTable3.java
@@ -1,7 +1,7 @@
 package HashTables;
 import java.util.ArrayList;
 
-public class HashTable3 {
+public class HashTable3 <K, V>{
     private class HashNode<K,V>{
         //we will use hashnodes for to handle collisions via chaining
         private K key;
@@ -43,14 +43,14 @@ public class HashTable3 {
         }
     }
 
-    private ArrayList<HashNode> list;
+    private ArrayList<HashNode<K,V>> list;
     private int bucketAmount;
     //size is different from bucketAmount because size is actually if there is anything in the list instead of null
     private int size;
 
     public HashTable3(int bucketAmount){
         this.bucketAmount = bucketAmount;
-        list = new ArrayList<HashNode>();
+        list = new ArrayList<HashNode<K,V>>();
         size = 0;
 
         for(int i = 0; i < bucketAmount; i ++){
@@ -58,6 +58,72 @@ public class HashTable3 {
         }
     }
 
-    
+    public int size(){
+        return size;
+    }
+    //this method gives us the index to insert for a given key from a hashnode
+    public int getIndex(K key){
+        //hashCode() can return a negative value if the key is too big use abs
+        int hashCode = Math.abs(key.hashCode());
+        int index = hashCode % bucketAmount;
+        return index;
+    }
 
+    public void add(K key, V val){
+        int ind = getIndex(key);
+        System.out.println(ind);
+        HashNode<K,V> head = list.get(ind);
+
+        while(head != null){
+            if(head.getKey() == key){
+                head.setValue(val);
+                return;
+            }
+            head = head.getNext();
+        }
+
+        head = list.get(ind);
+        HashNode<K,V> node = new HashNode<K,V>(key, val);
+        node.setNext(head);
+        list.set(ind, node);
+        size ++;
+
+        if((1.0 * size) / bucketAmount >= 0.7){
+            //double the array
+            ArrayList<HashNode<K,V>> temp = list;
+            list = new ArrayList<HashNode<K,V>>();
+            bucketAmount = 2 * bucketAmount;
+            size = 0;
+
+            for(int i = 0; i < bucketAmount; i ++){
+                list.add(null);
+            }
+
+            for (HashNode<K,V> h: temp) {
+                while(h.getNext() != null){
+                    add(h.getKey(), h.getValue());
+                    h = h.getNext();
+                }
+            }
+
+        }
+    }
+
+    //only prints the head node in the chain rn
+    public void print(){
+        for (HashNode<K,V> node: list) {
+            if (node == null) {
+                System.out.println("Node is null");
+            } else {
+                System.out.println(node.key + " " + node.getValue());
+            }
+        }
+    }
+    public static void main(String[] args) {
+        HashTable3<String,Integer> hT = new HashTable3<>(10);
+        hT.add("Matt", 123);
+        hT.add("asdfasdfsadsa", 222);
+        hT.print();
+
+    }
 }
