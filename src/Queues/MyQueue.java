3388f7e4efc8fb40fafd001bf5237d76d05443b0
Matthew Leong <mleong@skidmore.edu>
2018-05-07 18:29:37 -0400

    Added notes on my implementation against CtCI

--- src/Queues/MyQueue.java
+++ src/Queues/MyQueue.java
@@ -3,11 +3,17 @@ package Queues;
 import Stacks.Stack;
 import Stacks.StackNode;
 
+import java.util.NoSuchElementException;
+
 public class MyQueue<T> {
 
     private Stack<T> queue;
     private Stack<T> temp;
 
+    //This is good but not as good as Gayle
+    //if the queue is empty but the temp isn't we automatically move everything back into queue
+    //Gayle has is set up so that you always push to Queue. if the temp has elements always remove from there
+    //until it is empty. then refill it will elements from the queue.
     public void push(T data){
         //need to account for when temp is holding everything
         if(queue.isEmpty()){
@@ -26,5 +32,31 @@ public class MyQueue<T> {
             //if the queue isn't empty just put the new data in
             queue.push(data);
         }
+    }
+
+    public T remove(){
+        if(queue.isEmpty()){
+            if(temp.isEmpty()){
+                throw new NoSuchElementException();
+            } else {
+                T popData = temp.pop();
+                return popData;
+            }
+        } else {
+            //if the queue has elements transfer everything to temp to get the oldest item
+            while(!queue.isEmpty()){
+                T popData = queue.pop();
+                temp.push(popData);
+            }
+            return temp.pop();
+        }
+    }
+
+    public T peek(){
+        return queue.peek();
+    }
+
+    public boolean isEmpty(){
+        return queue.isEmpty() && temp.isEmpty();
     }
 }
