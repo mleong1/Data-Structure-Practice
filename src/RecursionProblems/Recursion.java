617dad72df8957074541c949db9082ce4ff7b22f
Matthew Leong <mleong@skidmore.edu>
2018-05-14 19:14:19 -0400

    Added a method to recursively compute n factorial

--- src/RecursionProblems/Recursion.java
+++ src/RecursionProblems/Recursion.java
@@ -186,6 +186,14 @@ public class Recursion {
         }
     }
 
+    public int factorial(int n) {
+        if(n <= 1){
+            return 1;
+        }
+        return n * factorial(n - 1);
+    }
+
+
     public static void main(String[] args) {
 
         int[] nums = {1, 5, 8, 11};
