3760726963f066545a788092790427564228b313
Matthew Leong <mleong@skidmore.edu>
2018-05-14 19:04:50 -0400

    Added a method to remove duplicate letters from a string

--- src/RecursionProblems/Recursion.java
+++ src/RecursionProblems/Recursion.java
@@ -175,6 +175,17 @@ public class Recursion {
                 + str.charAt(str.length() - 1);
     }
 
+    public String stringClean(String str) {
+        if(str.length() <= 1){
+            return str;
+        }
+        if(str.substring(0, 1).equals(str.substring(1, 2))){
+            return stringClean(str.substring(1));
+        } else {
+            return str.substring(0, 1) + stringClean(str.substring(1));
+        }
+    }
+
     public static void main(String[] args) {
 
         int[] nums = {1, 5, 8, 11};
